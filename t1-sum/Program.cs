c2631d17e4f562b6c6821e8afeb8aef565fc4f49
MahsanGilani <mahsan_gillani@yahoo.com>
2025-01-25 21:01:02 +0330

    program has been improved

--- t1-sum/Program.cs
+++ t1-sum/Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//initialize my numbers
+int num1 = 0;
+int num2 = 0;
 // get first number
 Console.WriteLine("Please write your first number: ");
-string Number1 = Console.ReadLine();
+string userinput = Console.ReadLine();
 // change string to integer
-int num1 = int.Parse(Number1);
+int num1 = int.Parse(userinput);
 //get second number
 Console.WriteLine("and your second number: ");
-string Number2 = Console.ReadLine();
+string userinput = Console.ReadLine();
 // change string to integer
-int num2 = int.Parse(Number2);
+int num2 = int.Parse(userinput);
 // calculating the sum
 Console.WriteLine($"sum = {num1 + num2}");
 
