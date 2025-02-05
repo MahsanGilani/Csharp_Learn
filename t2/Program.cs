1748f8bdb91d7ef9ef6c87fb0cd0f4668752d0f1
MahsanGilani <mahsan_gillani@yahoo.com>
2025-02-05 21:47:46 +0330

    Implicit Conversing Int to double

--- t2/Program.cs
+++ t2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Implicit Conversing
+int myInt = 124561;
+double myDouble = myInt;
+Console.WriteLine(myDouble);
