f423dc05c9f1fce89c4ca3d99c9e19b41a99415a
MahsanGilani <mahsan_gillani@yahoo.com>
2025-02-05 21:52:19 +0330

    Implicit conversing Int to long

--- t2/Program.cs
+++ t2/Program.cs
@@ -1,4 +1,9 @@
-﻿// Implicit Conversing
+﻿// Implicit Conversing for and Double
 int myInt = 124561;
 double myDouble = myInt;
 Console.WriteLine(myDouble);
+//-------------------------------------------------
+// Implicit coversing Int to long
+int myInt2 = 459894584;
+long myLong =  myInt2;
+Console.WriteLine(myLong);
