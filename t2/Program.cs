1fb4516749e0bdb82bd85ccbf2e48836c52e8e9c
MahsanGilani <mahsan_gillani@yahoo.com>
2025-02-05 21:53:35 +0330

    implicit conversing float to double

--- t2/Program.cs
+++ t2/Program.cs
@@ -7,3 +7,9 @@ Console.WriteLine(myDouble);
 int myInt2 = 459894584;
 long myLong =  myInt2;
 Console.WriteLine(myLong);
+//-------------------------------------------------
+// Implicit coversing float to double
+float myFloat = 123.123456789f;
+double mydouble2 = myFloat;
+Console.WriteLine(mydouble2);
+//-------------------------------------------------
