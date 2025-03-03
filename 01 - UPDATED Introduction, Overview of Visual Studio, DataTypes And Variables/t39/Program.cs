961ca59e5edfb31ebe36362575f465f182c74a00
MahsanGilani <mahsan_gillani@yahoo.com>
2025-03-03 19:37:48 +0330

    order of evaluation practise added

--- 01 - UPDATED Introduction, Overview of Visual Studio, DataTypes And Variables/t39/Program.cs
+++ 01 - UPDATED Introduction, Overview of Visual Studio, DataTypes And Variables/t39/Program.cs
@@ -11,3 +11,18 @@ var fullName2 = userName + userFamilyName;
 Console.WriteLine(fullName);
 Console.WriteLine(fullName2);
 //================================================
+// order of evaluation
+int num1 = 12;
+int num2 = 5;
+
+// concatenation
+Console.WriteLine("concatenation of both number is = " + num1 + num2);
+// addition
+Console.WriteLine("addition of both number is = "+ (num1 + num2));
+//subtraction
+Console.WriteLine("subttraction of both number is = " + (num1 - num2));
+//multiplication
+Console.WriteLine("multipulication of both number is = "+ (num1 * num2));   //اینجا اگه پرانتز هم نمیزاشتیم اوکی بود چون ضرب مقدم تره
+//division
+Console.WriteLine("division of both number is = "+ (num1/num2));
+//================================================
