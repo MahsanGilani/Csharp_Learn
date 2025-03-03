84ffe723a5d235594a8d061b206d7163e189c263
MahsanGilani <mahsan_gillani@yahoo.com>
2025-03-03 19:13:56 +0330

    implicitely type variable

---
+++ 01 - UPDATED Introduction, Overview of Visual Studio, DataTypes And Variables/t39/Program.cs
@@ -0,0 +1,13 @@
+﻿// implicitely type variable
+// var بر اساس اون مقداری که بهش میدیم، تایپش رو مشخص میکنه
+var userName = "Sara";
+var userFamilyName = "Austin";
+var userAge = 22;
+
+string fullName = userName + userFamilyName;
+// or
+var fullName2 = userName + userFamilyName;
+
+Console.WriteLine(fullName);
+Console.WriteLine(fullName2);
+//================================================
