9002dc3c486be8fec014321e9e723dedacdaceaa
MahsanGilani <mahsan_gillani@yahoo.com>
2025-05-02 12:34:46 +0330

    parse and readline added

--- 03 - Quera/t1/Program.cs
+++ 03 - Quera/t1/Program.cs
@@ -1,3 +1,13 @@
 ﻿// از تابع زیر برای متوجه شدن نوع متغیر استفاده میکنیم
-var number = 34;
-Console.WriteLine(number.GetType());
+//var number = 34;
+//Console.WriteLine(number.GetType());
+
+//**************************************************************
+//string str;
+//str = Console.ReadLine();
+//Console.WriteLine(str);
+
+//**************************************************************
+int num1;
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(num1);
