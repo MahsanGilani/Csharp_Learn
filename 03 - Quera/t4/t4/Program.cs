1e626ef73144a6bdadf5ec0f85687c21fa717d2f
MahsanGilani <mahsan_gillani@yahoo.com>
2025-05-24 21:01:52 +0330

    New practices for while and for iteration has been added

--- 03 - Quera/t4/t4/Program.cs
+++ 03 - Quera/t4/t4/Program.cs
@@ -192,3 +192,6 @@
 //Console.WriteLine(m);
 
 //***************************************************************
+string[] input = Console.ReadLine().Split(' ');
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
